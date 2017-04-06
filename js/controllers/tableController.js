d0f3365055dafdcf2f21e2223cd3eccf1bff9816
Chris Adkins <c.adkins19967@gmail.com>
2017-04-06 12:35:46 -0400

    initial commit

---
+++ js/controllers/tableController.js
@@ -0,0 +1,10 @@
+(function() {
+    'use strict';
+    angular
+        .module('shopular', [])
+        .controller('HeaderController', function() {
+
+        	const vm = this;
+			
+        });
+})();
